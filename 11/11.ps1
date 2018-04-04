67bf11e4b502ee7e2de24807a2ba6efb746c1100
Nic McElroy <nicamcelroy@gmail.com>
2018-04-04 21:13:48 +0100

    add solution to 11

--- 11/11.ps1
+++ 11/11.ps1
@@ -1,51 +1,60 @@
 class password {
     [string]$value
-    hidden [hashtable]$forward = @{}
-    hidden [hashtable]$back =@{}
-    hidden [array]$nums = 0..25
-    hidden [array]$chars = 97..122
-    hidden [int]$i =0
     #Constructor
     password([string]$value){
         $this.value = $value
-        foreach($char in $this.chars) {
-            $this.forward.Add("$($this.nums[$this.i])",([char]$char).ToString())
-            $this.back.Add(([char]$char).ToString(),$this.nums[$this.i])
-            $this.i++
-        }
     }
     [void] Iterate(){
         $temp = $this.value.ToCharArray()
-        ($temp.length -1)..0 | %{ 
-            if($_ -ne 0){
-                if($this.back["$($temp[$_])"] -lt 25){
-                    $newindex = $this.back["$($temp[$_])"] + 1
-                    $temp[$_] = $this.forward["$newindex"]
-                    $this.value = $temp -join ""
-                    break
+        $iterated = $false
+        $i = 0
+        while(!$iterated){
+            if($i -lt ($temp.Length -1)){
+                $currentchar = $temp[$temp.length - 1 - $i]
+                if([byte]$currentchar -lt 122){
+                    $newchar = [char]([byte]$currentchar +1)
+                    $temp[$temp.length -1 -$i ] = $newchar
+                    $iterated = $true
                 }
                 else{
-                    $temp[$_] = "a"
+                    $temp[$temp.length - 1 - $i] = [char]"a"
+                    $i++
                 }
             }
             else{
-                if($this.back["$($temp[$_])"] -lt 25){
-                    $newindex = $this.back["$($temp[$_])"] + 1
-                    $temp[$_] = $this.forward["$newindex"]
-                    $this.value = $temp -join ""
-                    break
-                }
-                else{
-                    $temp[$_] = "a"
-                    $this.value = "a" + [string]($temp -join "")
-                }
-
+                $temp = ("a"+($temp -join "")).ToCharArray()
+                $iterated = $true
             }
         }
+        $this.value = $temp -join ""
+    }
+    [bool] Validate(){
+        if($this.value -match "(\w)\1.*(\w)\2" -and $this.value -notmatch "(i|o|l)"){
+            if($this.value.length -gt 3){
+                foreach($index in 0..($this.value.Length -3)){
+                    if(([byte]$this.value.tochararray()[$index] -eq ([byte]$this.value.tochararray()[$index+1] -1)) -and ([byte]$this.value.tochararray()[$index] -eq ([byte]$this.value.tochararray()[$index+2] -2)) ){
+                        return $true
+                    }
+                    elseif($index -eq ($this.value.Length -3)){
+                        return $false
+                    }
+                }
+            }
+        }
+        else{
+            return $false
+        }
+        return $false
     }
 }
 
-$pass = [password]::new("z")
+$pass = [password]::new("cqjxjnds")
+while(!($pass.Validate())){
+    $pass.Iterate()
+}
 $pass
 $pass.Iterate()
+while(!($pass.Validate())){
+    $pass.Iterate()
+}
 $pass
