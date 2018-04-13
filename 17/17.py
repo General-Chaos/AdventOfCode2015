85bfdc88d54877f97eaa83da12f7816b489f66d8
Nic McElroy <nicamcelroy@gmail.com>
2018-04-13 17:35:12 +0100

    Upload tidyup

--- 17/17.py
+++ 17/17.py
@@ -3,31 +3,24 @@ containers = [11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1,
 
 
 def getcontainers(length, containers, passedlist=[]):
-    newcontainers = containers.copy()
     newlist = passedlist.copy()
-    container = newcontainers[0]
-    if container > length:
-        if len(newcontainers) > 1:
-            yield from getcontainers(length, newcontainers[1:], newlist)
-    elif container == length:
+    container = containers[0]
+    if container == length:
         yieldlist = passedlist.copy()
         yieldlist.append(container)
         yield yieldlist
-        if len(newcontainers) > 1:
-            yield from getcontainers(length, newcontainers[1:], newlist)
-    elif len(newcontainers) > 1:
-        yield from getcontainers(length, newcontainers[1:], newlist)
-        newlist.append(container)
-        yield from getcontainers((length - container), newcontainers[1:], newlist)
-    else:
-        pass
+    if len(containers) > 1:
+        yield from getcontainers(length, containers[1:], newlist)
+        if length > container:
+            newlist.append(container)
+            yield from getcontainers((length - container), containers[1:], newlist)
 
 allcombos = list(getcontainers(150, containers))
 
 print(f"Answer 1 is: {len(allcombos)}")
 
 min = min(map(len, allcombos))
-count = 0 
+count = 0
 for i in allcombos:
     if len(i) == min:
         count += 1
