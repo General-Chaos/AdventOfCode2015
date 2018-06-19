3df15fd1417aa89c385b81c0b4ab6523bcba0505
Nic McElroy <nicamcelroy@gmail.com>
2018-06-20 00:40:22 +0100

    add part 2

--- 20/20.py
+++ 20/20.py
@@ -32,3 +32,36 @@ for i in get_scores():
     else:
         print(f"The answer to 1 is: {i[0]}")
         break
+
+
+# For part 2 we need to actually get the factors and discard any scores from factors below i/50
+def get_uniquefactors(number):
+    primefactors = list(get_primefactors(number))
+    uniquefactors = set()
+    for i in range(len(primefactors) + 1):
+        for j in itertools.combinations(primefactors, i):
+            r = 1
+            for k in j:
+                r *= k
+            uniquefactors.add(r)
+    return uniquefactors
+
+
+def get_scores_2():
+    i = 1
+    while True:
+        base = i / 50
+        score = 0
+        for j in get_uniquefactors(i):
+            if j >= base:
+                score += (j * 11)
+        yield (i, score)
+        i += 1
+
+
+for i in get_scores_2():
+    if i[1] < 29000000:
+        pass
+    else:
+        print(f"The answer to 2 is: {i[0]}")
+        break
